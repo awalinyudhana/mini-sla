4d37fdba1e2f788fbbdd5bd6c607ce7428a84931
Mafaik <mafaikmon@gmail.com>
2017-02-07 10:13:27 +0700

    CRUD Perangkat

--- application/views/admin/themes/footer.php
+++ application/views/admin/themes/footer.php
@@ -12,7 +12,67 @@
 </div>
 <!-- /#wrapper -->
 
+<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                Konfirmasi Hapus Data
+            </div>
+            <div class="modal-body">
+                <p>Apakah anda yakin menghapus data ini?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+                <a class="btn btn-danger btn-ok">Hapus</a>
+            </div>
+        </div>
+    </div>
+</div>
+
 <!-- Core Scripts - Include with every page -->
+<script type="text/javascript">
+var table;
+ 
+$(document).ready(function() {
+ 
+    //datatables
+    table = $('#datatable').DataTable({ 
+ 
+        "processing": true, //Feature control the processing indicator.
+        "serverSide": true, //Feature control DataTables' server-side processing mode.
+        "order": [], //Initial no order.
+ 
+        // Load data for the table's content from an Ajax source
+        "ajax": {
+            "url": "<?php echo site_url('perangkat/ajax_list')?>",
+            "type": "POST"
+        },
+ 
+        //Set column definition initialisation properties.
+        "columnDefs": [
+        { 
+            "targets": [ 0 ], //first column / numbering column
+            "orderable": false, //set not orderable
+        },
+        ],
+ 
+    });
+
+    
+ 
+});
+
+var aElems = document.getElementsByClassName('delete-confirmation');
+
+    for (var i = 0, len = aElems.length; i < len; i++) {
+        aElems[i].onclick = function() {
+            return confirm("Are you sure you want to leave?");
+        };
+    }
+$('#confirm-delete').on('show.bs.modal', function(e) {
+    $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
+});
+</script>
 </body>
 
 </html>
---
+++ application/views/perangkat/index.php
@@ -0,0 +1,54 @@
+<div id="page-wrapper">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header"> Data Perangkat</h1>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-lg-12">
+            <!-- /.panel -->
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <i class="fa fa-bar-chart-o fa-fw"></i> SiMiLa | Daftar Data Perangkat
+                </div>
+
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <?php if (isset($message)) {
+                                ?>
+                                <div class="alert alert-warning"><?php echo $message; ?></div>
+                                <?php
+                            } ?>
+
+                            <p align="right">
+                                <?php echo anchor('perangkat/create', 'Tambah Perangkat Baru', "class='btn btn-success'") ?>
+                            </p>
+
+                            <table cellpadding=0 cellspacing=10 class="table" id="datatable">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Part Number</th>
+                                        <th>Brand</th>
+                                        <th>Nama Perangkat</th>
+                                        <th>Type</th>
+                                        <th>Status</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody></tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+                </div>
+            </div>
+            <!-- /.panel-heading -->
+            <!-- /.panel-body -->
+        </div>
+        <!-- /.panel -->
+    </div>
+</div>
