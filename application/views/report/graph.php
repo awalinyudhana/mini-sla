cf135f2702ec2fe036b659306b2243025131211a
Mafaik <mafaikmon@gmail.com>
2017-05-22 06:45:47 +0700

    revisi 6 Mei #2

--- application/views/report/graph.php
+++ application/views/report/graph.php
@@ -1,7 +1,7 @@
 <div id="page-wrapper">
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header"> Daftar Ticket</h1>
+            <h1 class="page-header"> Grafik Ticket</h1>
         </div>
         <!-- /.col-lg-12 -->
     </div>
@@ -22,10 +22,25 @@
                             <?php } ?>
                         </div>
                     </div>
-                    <?php if (isset($type) && ($type == 'overdue')) { ?>
                     <div class="row">
                         <div class="col-lg-6 pull-right">
                             <form class="form-horizontal">
+                                <?php if (isset($type) && $type == 'category') { ?>
+                                <div class="form-group">
+                                    <label class="col-sm-6 control-label">Kategori</label>
+                                    <div class="col-sm-6">
+                                        <select name="category" id="category" class="form-control">
+                                            <option value="Installation" <?php if ($category=='Installation') { ?>selected<?php } ?>>Installation</option>
+                                            <option value="Maintenance" <?php if ($category=='Maintenance') { ?>selected<?php } ?>>Maintenance</option>
+                                            <option value="Support" <?php if ($category=='Support') { ?>selected<?php } ?>>Support</option>
+                                            <option value="MoM" <?php if ($category=='MoM') { ?>selected<?php } ?>>MoM</option>
+                                            <option value="BoM" <?php if ($category=='BoM') { ?>selected<?php } ?>>BoM</option>
+                                            <option value="Demo" <?php if ($category=='Demo') { ?>selected<?php } ?>>Demo</option>
+                                            <option value="Doc" <?php if ($category=='Doc') { ?>selected<?php } ?>>Doc</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <?php } ?>
                                 <div class="form-group">
                                     <label class="col-sm-6 control-label">Tanggal Mulai</label>
                                     <div class="col-sm-6">
@@ -57,35 +72,12 @@
                     </div>
                     <div class="row">
                         <div class="col-lg-12">
-                            <button class="btn btn-default pull-right" style="margin-bottom: 15px;" id="buttonFilter">Filter Overdue</button>
-                            <a href="<?php echo base_url();?>ticket_list/print_overdue" target="_blank" class="btn btn-default pull-right" style="margin-right: 10px;">Cetak Overdue Ticket</a>
+                            <button class="btn btn-default pull-right" style="margin-bottom: 15px;" id="buttonFilter">Filter Data</button>
                         </div>
                     </div>
-                    <?php } ?>
                     <div class="row">
                         <div class="col-lg-12">
-                            <table cellpadding=0 cellspacing=10 class="table" id="datatable" data-url="<?php echo $table_url; ?>">
-                                <thead>
-                                    <tr>
-                                        <th>No</th>
-                                        <th>No. Ticket</th>
-                                        <th>Tanggal</th>
-                                        <th>Judul</th>
-                                        <th>Customer</th>
-                                        <th>Perangkat</th>
-                                        <th>Kategori</th>
-                                        <th>Request By</th>
-                                        <!-- <th>Tech. Status</th> -->
-                                        <th>Status Teknisi</th>
-                                        <th>Persetujuan Pemimpin</th>
-                                        <?php if (isset($type) && ($type == 'hasaction' || $type == 'overdue')) { ?>
-                                            <th>Aksi</th>
-                                        <?php } ?>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                </tbody>
-                            </table>
+                            <div id="graph-container" style="min-width: 310px;height: 400px;margin: 0 auto"></div>
                         </div>
                     </div>
                     <!-- /.row -->
@@ -101,12 +93,51 @@
     document.getElementById("buttonFilter").addEventListener("click", function () {
         var start = $("#start_date_of_support").val();
         var end = $("#end_date_of_support").val();
+
+        <?php if (isset($type) && $type == 'category') { ?>
+        var category = $("#category").val();
+        // var target = <?php echo base_url(); ?>+"report/ticket_graph_by_category/"+category+'/'+start+'/'+end
+        <?php } ?>
         if (start && end) {
             var start_date = new Date(start);
             var end_date = new Date(end);
-            if (start_date < end_date) {
-                window.location = "<?php echo base_url(); ?>ticket_list/overdue/"+start+'/'+end;
-            }
+        } else {
+            var start_date = '';
+            var end_date = '';
         }
+            
+        <?php if (isset($type) && $type == 'category') { ?>
+            window.location = "<?php echo base_url(); ?>report/ticket_graph_by_category/"+category+'/'+start+'/'+end;
+        <?php } else { ?>
+            window.location = "<?php echo base_url(); ?>report/ticket_graph/"+start+'/'+end;
+        <?php } ?>
     }, false);
+
+    Highcharts.chart('graph-container', {
+
+        title: {
+            text: 'Grafik Ticket'
+        },
+
+        yAxis: {
+            title: {
+                text: 'Jumlah Request'
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle'
+        },
+
+        xAxis: {
+            categories:[<?php foreach ($data as $key => $value) {echo "'".$value->tahun."-".$value->bulan."',";}?>]
+        },
+
+        series: [{
+            name: 'Grafik Ticket',
+            data: [<?php foreach ($data as $key => $value) {echo $value->jumlah.",";}?>]
+        }]
+
+    });
 </script>
